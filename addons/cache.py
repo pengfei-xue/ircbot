870caf19d5f35624755eb36a57d117ff95a39629
Pengfei Xue <pengphy@gmail.com>
2012-11-08 16:39:51 +0800

    add expire time for cache

--- addons/cache.py
+++ addons/cache.py
@@ -1,12 +1,29 @@
 # -*- coding: utf8 -*-
 
+import time
+
 class Cacher(object):
-    def __init__(self):
+    def __init__(self, expired=3600):
         self._cached_data = {}
+        self.expired = expired
 
     def get(self, key):
-        cached_value = self._cached_data.get(key, None)
+        now = time.time()
+        old_ts = self._cached_data[key]['timestamp']
+
+        if now - old_ts >= self.expired:
+            return None
+        
+        cached_value = self._cached_data[key]['value']
         return cached_value
 
     def set(self, key, value):
-        self._cached_data[key] = value
+        self._cached_data[key] = {}
+        self._cached_data[key]['value'] = value
+        self._cached_data[key]['timestamp'] = time.time()
+
+    def refresh(self):
+        self._cached_data = {}
+
+    def retire(self, key):
+        self._cached_data[key] = dict.fromkeys(['value', 'timestamp'])
